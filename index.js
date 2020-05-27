37c59036a9070ba75d4845607f209c28d03a0f99
ivanlevendyan <ivan.levendyan@gmail.com>
2020-05-27 16:01:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 let fs = require('fs');
 let path = require("path");
 
-module.exports.resume = (event, callback) => {
+module.exports.resume = (data, context, callback) => {
 
     const obj = fs.readFileSync('./resume.json', 'utf8', function(err, data) {
         if(err) {
