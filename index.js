3a25ad223b685dcd6cbcdba3b282357f0ae265ab
ivanlevendyan <ivan.levendyan@gmail.com>
2020-05-28 09:45:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,11 @@
 let escapeHtml = require('escape-html');
 let express = require('express');
 
+module.exports.resume = (req, res) => {
 
-var app = express();
-app.get('/test', function(req, res) {
+    var app = express();
+    app.get('/test', function(req, res) {
     res.sendFile('./resume.html', {root: __dirname })
-});
+    });
+    
+};
