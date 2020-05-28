4853fef4e3a832ccdb4f2183a8b1d6ed6f5829c1
ivanlevendyan <ivan.levendyan@gmail.com>
2020-05-28 10:13:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ let escapeHtml = require('escape-html');
 
 module.exports.resume = (req, res) => {
 
-    fs.readFileSync('./resume.html', 'utf8', function(err, data) {
-        if (!err) {
-            console.log('received data: ' + data);
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            res.end();
-        } else {
-            console.log(err);
+    const obj =  fs.readFileSync('./resume.html', 'utf8', function(err, data) {
+        if (err) {
+                callback(err);
         }
         
     });
+    
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write(obj);
+ res.end();
     
 };
