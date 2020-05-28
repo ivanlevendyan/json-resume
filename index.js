f45ed649ceceadceb5111878f00a4c6ec619f247
ivanlevendyan <ivan.levendyan@gmail.com>
2020-05-28 10:00:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ module.exports.resume = (req, res) => {
     const obj = fs.readFileSync('./resume.html', 'utf8', function(err, data) {
         if (!err) {
             console.log('received data: ' + data);
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(data);
-            response.end();
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
         } else {
             console.log(err);
         }
