98fdb180a8a33847093a7dd967eb40a70e33e9b1
ivanlevendyan <ivan.levendyan@gmail.com>
2020-05-28 09:35:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 
 'use strict';
 
-let fs = require('fs');
-let path = require("path");
 let escapeHtml = require('escape-html');
+let express = require('express');
 
-module.exports.resume = (req, res) => {
 
-    const obj = fs.readFileSync('./resume.json', 'utf8', function(err, data) {
-        if(err) {
-            callback(err);
-        }
-        
-    });
-
-    const fdata = require('./resume.json')
-    
-    const response = {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "text/json"
-        },
-        "body": obj
-    };
-    
- 
-
-res.writeHead(200, {'Content-Type': 'text/plain'});
- res.write(data);
- res.end();
-
-    
-};
+var app = express();
+app.get('/test', function(req, res) {
+    res.sendFile('./resume.html', {root: __dirname })
+});
